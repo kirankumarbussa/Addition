3cb792cd96b3f18fd531d451edc575239cfd96d6
Kiran Bussa <bussasunny72@gmail.com>
2018-02-04 09:40:16 +0530

    calculaor add functionality to project

---
+++ src1/Addition.java
@@ -0,0 +1,12 @@
+class Addition {
+	public static void main(String[] args) {
+		int result = addition(10,12);
+		System.out.println(result);
+	}
+
+        public static int addition(int num1, int num2){
+		return num1 + num2;
+
+    }
+}
+
